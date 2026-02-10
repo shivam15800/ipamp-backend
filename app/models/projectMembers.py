2fa7e19fc082f9fa609f49a937fb3a3c3c63d2ab
shivam <your-email@example.com>
2026-02-11 04:49:23 +0530

    table name chnged from project_memers to projectmembers

--- app/models/projectMembers.py
+++ app/models/projectMembers.py
@@ -2,6 +2,6 @@ from sqlalchemy import Column, Integer, ForeignKey
 from . import Base
 
 class ProjectMembers(Base):
-    __tablename__="project_members"
+    __tablename__="projectmembers"
     project_id = Column(Integer, ForeignKey("projects.id"), primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
